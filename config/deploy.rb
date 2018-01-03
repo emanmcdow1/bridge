8351b790f2d501d2f6169f53fcdad4fb17e1483e
Emmett McDow <emanmcdow@gmail.com>
2018-01-03 12:59:06 -0500

    Set Capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,19 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
-
+set :application, "Bridge"
+set :repo_url, "https://github.com/emanmcdow1/bridge"
+set :deploy_to, "home/admin"
+set :scm, :git
+set :branch, "master"
+set :user, "admin"
+set :use_sudo, false
+set :rails_env, "production"
+set :deploy_via, :copy
+set :ssh_options, {:forward_agent => true, :port => 7822}
+set :keep_releases, 5
+default_run_options[:pty] = true
+server "70.32.24.246", :app, :web, :db, :primary => true
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
