f4c5e721db0df9e80190e1ef988fb86466fe1c4b
Emmett McDow <emanmcdow@gmail.com>
2018-01-03 13:24:15 -0500

    set user for cap ssh

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,13 +49,13 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
+ server "70.32.24.246",
+   user: "admin",
+   roles: %w{70.32.24.246},
+   ssh_options: {
+     user: "admin", # overrides user setting above
+     keys: %w(/home/user_name/.ssh/id_rsa),
+     forward_agent: false,
+     auth_methods: %w(publickey password)
+     # password: "please use keys"
+   }
