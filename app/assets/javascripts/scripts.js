22f2926b2d5ef7e3bd648dfc93e924292ad2ec1d
emanmcdow1 <emanmcdow@gmail.com>
2018-02-02 11:35:09 -0500

    Fixed Menu Bar

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,24 +1,67 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+$( document ).on('turbolinks:load', function() {
 
-aside = false;
-var i = 0;
-var j = 0;
-var hvr = false;
+  aside = false;
+  var i = 0;
+  var j = 0;
+  var hvr = false;
+  var first = true;
+  var mobile = false;
+  let moving = false;
 
-//var outerPane = $details.find(".details-pane-outer"),
-didScroll = false;
+  //var outerPane = $details.find(".details-pane-outer"),
+  didScroll = false;
 
-$(window).scroll(function() {
-    didScroll = true;
-});
-
-var first = true;
-var mobile = false;
+  $(window).scroll(function() {
+      didScroll = true;
+  });
 
   positionBLK = ($('.active').position().left)+(($('.active').width())/2)-($('.blk').width()/2);
   $('.blk').css('left', positionBLK);
   $('#suprise .active').slideUp('fast');
   $("#suprise a:not('.active')").css('display', 'none');
+
+
+  var animatebar = function(){
+
+
+    if ((didScroll || hvr) && !mobile && !moving) {
+      didScroll = false;
+      //console.log(pACT);
+      scroll = $(window).scrollTop();
+
+      if(scroll < 25){
+        $('.blkbox').stop().animate({backgroundColor: "rgba(0,0,0,0)"}, 600);
+        $('#links').each(function(){
+          $(this).stop().slideDown("fast");
+        });
+        $('.blk').stop().animate({left: positionBLK}, 1100);
+        $('#suprise .active').stop().slideUp('fast');
+        moving = true;
+      };
+      if(scroll > 25 && hvr){
+        $('.blkbox').stop().animate({backgroundColor: "rgba(0,0,0,.5)"}, 600);
+        $('#links').each(function(){
+          $(this).stop().slideDown("fast");
+        });
+        $('.blk').stop().animate({left: positionBLK}, 1100);
+        $('#suprise .active').slideUp('fast');
+        hvr = false;
+        moving = true;
+      }else if(scroll > 25 && !hvr){
+        $('.blkbox').stop().animate({backgroundColor: "rgba(0,0,0,0)"}, 600);
+        $('#links').each(function(){
+          $(this).stop().slideUp("fast");
+        });
+        $('.blk').stop().animate({left: "79.3vw"}, 1100);
+        $('#suprise .active').stop().slideDown('fast');
+        moving = true;
+      };
+    };
+    var def = new $.Deferred();
+     def.resolve(false);
+     return def;
+
+  };
 
   setInterval(function() {
 
@@ -36,38 +79,7 @@ var mobile = false;
 
     var scroll = $(window).scrollTop();
 
-
-    if ( (didScroll || hvr)&& !mobile) {
-        didScroll = false;
-        //console.log(pACT);
-        scroll = $(window).scrollTop();
-
-        if(scroll < 25){
-          $('.blkbox').stop().animate({backgroundColor: "rgba(0,0,0,0)"}, 600);
-          $('#links').each(function(){
-            $(this).stop().slideDown("fast");
-          });
-          $('.blk').stop().animate({left: positionBLK}, 1100);
-          $('#suprise .active').stop().slideUp('fast');
-        };
-        if(scroll > 25 && hvr){
-          $('.blkbox').stop().animate({backgroundColor: "rgba(0,0,0,.5)"}, 600);
-          $('#links').each(function(){
-            $(this).stop().slideDown("fast");
-          });
-          $('.blk').stop().animate({left: positionBLK}, 1100);
-          $('#suprise .active').slideUp('fast');
-          hvr = false;
-        }else if(scroll > 25 && !hvr){
-          $('.blkbox').stop().animate({backgroundColor: "rgba(0,0,0,0)"}, 600);
-          $('#links').each(function(){
-            $(this).stop().slideUp("fast");
-          });
-          $('.blk').stop().animate({left: "79.3vw"}, 1100);
-          $('#suprise .active').stop().slideDown('fast');
-        };
-
-    };
+    animatebar().done(function(){moving = false;});
 
   }, 700);
 
--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -4,7 +4,7 @@ $('.carousel').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 2000,
+  autoplaySpeed: 3000,
   prevArrow:"<div class='a-left control-c prev slick-prev hvr-grow' src='' id='leftb'></div>",
   nextArrow:"<div class='a-right control-c next slick-next hvr-grow' src='' id='rightb'></div>"
 
