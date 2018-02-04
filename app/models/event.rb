b0132bc9f276dccf7e5f2d13c8a6f096a889ea87
emanmcdow1 <emanmcdow@gmail.com>
2018-02-04 17:30:16 -0500

    paperclip

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,5 @@
 class Event < ApplicationRecord
   has_attached_file :background,
-    :url => "/system/:attachment/:id/:style/:filename",
     :default_url => "missing.png"
     validates_attachment_content_type :background, :content_type => ["image/jpg", "image/jpeg", "image/gif", "image/png"]
 end
