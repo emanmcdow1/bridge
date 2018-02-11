a67bb645970262e11200174ae0ad92bbc3bb7e35
emanmcdow1 <emanmcdow@gmail.com>
2018-02-11 08:10:46 -0500

    fix domain

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+SitemapGenerator::Sitemap.default_host = "http://thebridge4all.org"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
