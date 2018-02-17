2868f989d6ca0c3ab4d5447ed6e6ab05a59727b4
emanmcdow1 <emanmcdow@gmail.com>
2018-02-17 06:04:42 -0500

    Security finally

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
-  resources :events, :sermons
+  resources :events, :sermons, :users
 
   get 'pages/home'
 
@@ -21,15 +21,14 @@ Rails.application.routes.draw do
 
   get 'pages/groups'
 
+
+  get    '/signup',  to: 'users#new'
+
   get    '/login',   to: 'sessions#new'
 
   post   '/login',   to: 'sessions#create'
-
+  
   delete '/logout',  to: 'sessions#destroy'
-
-  get '/signup' => 'users#new'
-
-  post '/users' => 'users#create'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
